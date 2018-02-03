976c746aaa739bff526f3e0b9421ebb53194cea3
Daniel Oh <daniel.ej.oh@gmail.com>
2018-02-03 16:09:11 -0500

    Add files via upload

---
+++ python-crash-course/exercises/chapter-9/9-6-ice-cream-stand.py
@@ -0,0 +1,66 @@
+class Restaurant():
+    """A simple attempt to model a restaurant"""
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initialize restaurant name and cuisine type attributes"""
+        
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    
+    def describe_restaurant(self):
+        """
+        Describe the name of the restaurant and what kind of cuisine
+        it serves.
+        """
+        print(self.restaurant_name.title() + " serves " + 
+            self.cuisine_type.title() + ".")
+    
+    def open_restaurant(self):
+        """
+        Display a message stating that the restaurant is open.
+        """
+        print("The " + self.restaurant_name.title() + " is open!")
+    
+    def set_number_served(self, number_served):
+        """
+        Set the number of customers served.
+        """
+        if number_served >= 0:
+            self.number_served = number_served
+        else:
+            print("Must be a value greater than 0.")
+            
+    def increment_number_served(self, number_served):
+        """
+        Increment the number of customers served.
+        """
+        if number_served >= 0:
+            self.number_served += number_served
+        else:
+            print("Must be a value greater than 0.")
+        
+class IceCreamStand(Restaurant):
+    """Represents an ice cream stand, a subset of restaurant"""
+    def __init__(self, restaurant_name, cuisine_type):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes of an Ice Cream Stand
+        Set 3 default flavors in a list
+        """
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['chocolate', 'vanilla', 'strawberry']
+            
+    def display_flavors(self):
+        """Display the flavors offered by the restaurant."""
+        print(self.restaurant_name.title() + " serves: ")
+        for flavor in self.flavors:
+            print("\t-" + flavor.title())
+
+ben_and_jerrys = IceCreamStand('ben and jerrys', 'dessert')
+ben_and_jerrys.display_flavors()
+
+        
+    
+    
+    
---
+++ python-crash-course/exercises/chapter-9/9-7-admin.py
@@ -0,0 +1,51 @@
+class User():
+    """
+    A simple attempt to model a User
+    """
+    
+    def __init__(self, first_name, last_name, age, gender, location):
+        """
+        Initialize first name, last name, age, gender and location
+        for user
+        """
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.gender = gender
+        self.location = location
+        
+    def describe_user(self):
+        """ Prints a summary of the user profile """
+        print("Name: " + self.first_name.title() + " "
+            + self.last_name.title())
+        print("Age: " + str(self.age))
+        print("Gender: " + self.gender.title())
+        print("Location: " + self.location.title())
+    
+    def greet_user(self):
+        """Prints a personalized greeting to user"""
+        print("Hello " + self.first_name.title() + " " +
+            self.last_name.title() + ". I hope "
+            + self.location.title() + " is treating you well.")
+
+class Admin(User):
+    """Represents an Admin, a subset of User"""
+    def __init__(self, first_name, last_name, age, gender, location):
+        """
+        Initialize attributes of the parent class. 
+        Then initalize attributes of an Admin user
+        """
+        super().__init__(first_name, last_name, age, gender, location)
+        self.privileges = ['can add post', 'can delete post'
+            , 'can ban user']
+    def show_privileges(self):
+        print(self.first_name.title() + ' ' + self.last_name.title()
+            + ' has the following privileges: ')
+        for privilege in self.privileges:
+            print('\t' + str(privilege))
+
+superuser = Admin('Damon', 'Jones', 38, 'Male', 'New York')
+superuser.show_privileges()
+            
+        
